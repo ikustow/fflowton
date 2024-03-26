bf15ead3e87a4c31c80ef408305a5c79f9f8015e
Ilia Kustov <36426291+ikustow@users.noreply.github.com>
2024-03-26 08:02:44 +0100

    Add files via upload

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,18 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "fb44f56a48fd65c659174cc588527ad5",
-"assets/AssetManifest.bin.json": "8b9becbe6e130a4a193651596c1893b0",
-"assets/AssetManifest.json": "bcc05f8ca7622edddbf6a01984995cf3",
+const RESOURCES = {"assets/AssetManifest.bin": "c13c0b2475e072eff05afad7d8f65381",
+"assets/AssetManifest.bin.json": "2cb0ddb5a4fc27d0c05396355c6d5216",
+"assets/AssetManifest.json": "eb71ce8f48a16879a4ace5242aa012fc",
 "assets/assets/audios/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/assets/fonts/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"assets/assets/images/5611220_9fae.jpg": "f115a7e827488893301b81c60cb7f731",
 "assets/assets/images/CAT_emoji_icon_png_grande.webp": "3f1629bb7cb586996dce6814c6a1deac",
 "assets/assets/images/charity.webp": "162848263c240a142a70a25aad91d4bb",
 "assets/assets/images/donate.webp": "6c541e44bbb8538d686b1c7bb46e8539",
 "assets/assets/images/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/assets/images/idGj5i8Kzv.jpeg": "01db2631bb67c83cc99900c210675b98",
 "assets/assets/images/sport.webp": "8e27bff96134cae92abf5f51c64abf59",
+"assets/assets/images/supabase-logo.svg": "969c45df5054eed630ac9cdafceb854e",
 "assets/assets/images/ton.png": "5f9c1fbebd9de343832d1621f74bc5fc",
 "assets/assets/images/toncoin-ton-coin-stacks-cryptocurrency-3d-render-illustration-free-png.webp": "fd08a82b26cc04ee880da3829a95976a",
 "assets/assets/images/unnamed.png": "bdaf7868c29da7453a7c57d376745214",
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fb44f56a48fd65c659174cc588527ad5
 "assets/assets/videos/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "eefa88b135c7dfe787fad6db7c3bb778",
-"assets/NOTICES": "f1bc4ce9def445e09ae04ba21c5a4cda",
+"assets/NOTICES": "1b33bd077d5fc96711bdc80da2329898",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "f25e8e701660fb45e2a81ff3f43c6d5c",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "a5d7457fda15b7622c14f432ba63039a",
@@ -44,9 +44,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "fb44f56a48fd65c659174cc588527ad5
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f5f8300a6c44409ec90dd2908bc69a8d",
-"/": "f5f8300a6c44409ec90dd2908bc69a8d",
-"main.dart.js": "2a6e618182e20d5eab67907b94123933",
+"index.html": "390852cd2481fd88b07ef87bb54494bc",
+"/": "390852cd2481fd88b07ef87bb54494bc",
+"main.dart.js": "e7ff6b85c2ba1214d12e1e66415dad6a",
 "version.json": "512f03836e80c604df6833f59e7444eb"};
 // The application shell files that are downloaded before a service worker can
 // start.
